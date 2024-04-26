f4c1a55211c9446e4625c0a1babad7cca3500852
Saurabh <Saurabh@RAJE>
2024-04-26 14:23:14 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Textnumber = New System.Windows.Forms.TextBox()
+        Me.Textaddress = New System.Windows.Forms.TextBox()
+        Me.Textname = New System.Windows.Forms.TextBox()
+        Me.Textemail = New System.Windows.Forms.TextBox()
+        Me.Textid = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1069, 66)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(448, 22)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(197, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Crud Opration"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.DateTimePicker1)
+        Me.Panel2.Controls.Add(Me.Button4)
+        Me.Panel2.Controls.Add(Me.Button3)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Textnumber)
+        Me.Panel2.Controls.Add(Me.Textaddress)
+        Me.Panel2.Controls.Add(Me.Textname)
+        Me.Panel2.Controls.Add(Me.Textemail)
+        Me.Panel2.Controls.Add(Me.Textid)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Location = New System.Drawing.Point(18, 90)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1033, 339)
+        Me.Panel2.TabIndex = 1
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(840, 27)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(147, 31)
+        Me.Button4.TabIndex = 15
+        Me.Button4.Text = "Delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(685, 28)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(147, 31)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(531, 30)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(147, 31)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Insert"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(376, 28)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 31)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Textnumber
+        '
+        Me.Textnumber.Location = New System.Drawing.Point(159, 244)
+        Me.Textnumber.Margin = New System.Windows.Forms.Padding(4)
+        Me.Textnumber.Name = "Textnumber"
+        Me.Textnumber.Size = New System.Drawing.Size(160, 22)
+        Me.Textnumber.TabIndex = 11
+        '
+        'Textaddress
+        '
+        Me.Textaddress.Location = New System.Drawing.Point(159, 202)
+        Me.Textaddress.Margin = New System.Windows.Forms.Padding(4)
+        Me.Textaddress.Name = "Textaddress"
+        Me.Textaddress.Size = New System.Drawing.Size(160, 22)
+        Me.Textaddress.TabIndex = 10
+        '
+        'Textname
+        '
+        Me.Textname.Location = New System.Drawing.Point(159, 76)
+        Me.Textname.Margin = New System.Windows.Forms.Padding(4)
+        Me.Textname.Name = "Textname"
+        Me.Textname.Size = New System.Drawing.Size(160, 22)
+        Me.Textname.TabIndex = 8
+        '
+        'Textemail
+        '
+        Me.Textemail.Location = New System.Drawing.Point(159, 121)
+        Me.Textemail.Margin = New System.Windows.Forms.Padding(4)
+        Me.Textemail.Name = "Textemail"
+        Me.Textemail.Size = New System.Drawing.Size(160, 22)
+        Me.Textemail.TabIndex = 7
+        '
+        'Textid
+        '
+        Me.Textid.Location = New System.Drawing.Point(159, 34)
+        Me.Textid.Margin = New System.Windows.Forms.Padding(4)
+        Me.Textid.Name = "Textid"
+        Me.Textid.Size = New System.Drawing.Size(160, 22)
+        Me.Textid.TabIndex = 6
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(17, 238)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(123, 31)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Enumber"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(17, 196)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(122, 31)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Eaddress"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(17, 155)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(76, 31)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Edob"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(17, 114)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 31)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Eemail"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(17, 70)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 31)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Ename"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(17, 28)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 31)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Id"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(17, 438)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.Size = New System.Drawing.Size(1033, 239)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(159, 164)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker1.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1067, 692)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Crud Opration"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Textname As TextBox
+    Friend WithEvents Textemail As TextBox
+    Friend WithEvents Textid As TextBox
+    Friend WithEvents Textnumber As TextBox
+    Friend WithEvents Textaddress As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
---
+++ Form1.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim con As New SqlConnection("Data Source=RAJE\SQLEXPRESS;Initial Catalog=crudEmp;Integrated Security=True;")
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ValidateInput() Then
+            Dim id As Integer = Textid.Text
+            Dim ename As String = Textname.Text
+            Dim enumber As Integer = Convert.ToInt32(Textnumber.Text)
+            Dim eemail As String = Textemail.Text
+            Dim eaddress As String = Textaddress.Text
+            Dim edob As String = DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")
+
+            con.Open()
+            Dim command As New SqlCommand("insert into emp_Tab values('" & id & "','" & ename & "','" & enumber & "','" & eemail & "','" & eaddress & "','" & edob & "')", con)
+            command.ExecuteNonQuery()
+            con.Close()
+
+            MessageBox.Show("Record Successfully Inserted")
+            LoadDataInGrid()
+        End If
+    End Sub
+
+    Private Sub LoadDataInGrid()
+        Dim command As New SqlCommand("select * from emp_Tab", con)
+        Dim sds As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sds.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Function ValidateInput() As Boolean
+        If String.IsNullOrWhiteSpace(Textid.Text) Then
+            MessageBox.Show("Please enter an ID.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If String.IsNullOrWhiteSpace(Textname.Text) Then
+            MessageBox.Show("Please enter a name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If String.IsNullOrWhiteSpace(Textemail.Text) Then
+            MessageBox.Show("Please enter an email.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If String.IsNullOrWhiteSpace(Textaddress.Text) Then
+            MessageBox.Show("Please enter an address.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If DateTimePicker1.Value > DateTime.Now Then
+            MessageBox.Show("Date of birth cannot be in the future.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        If String.IsNullOrWhiteSpace(Textnumber.Text) OrElse Not IsNumeric(Textnumber.Text) Then
+            MessageBox.Show("Please enter a valid number.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataInGrid()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If ValidateInput() Then
+            Dim id As Integer = Textid.Text
+            Dim ename As String = Textname.Text
+            Dim enumber As Integer = Convert.ToInt32(Textnumber.Text)
+            Dim eemail As String = Textemail.Text
+            Dim eaddress As String = Textaddress.Text
+            Dim edob As String = DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")
+
+            con.Open()
+            Dim command As New SqlCommand("update emp_Tab set ename = '" & ename & "', enumber = '" & enumber & "', eemail = '" & eemail & "', eaddress = '" & eaddress & "', edob = '" & edob & "' where id = '" & id & "'", con)
+            command.ExecuteNonQuery()
+            con.Close()
+
+            MessageBox.Show("Record Successfully Updated")
+            LoadDataInGrid()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If MessageBox.Show("Are You Sure To Delete?", "Delete Document", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+            Dim id As Integer = Textid.Text
+            con.Open()
+            Dim command As New SqlCommand("delete from emp_Tab where id = '" & id & "'", con)
+            command.ExecuteNonQuery()
+            con.Close()
+            MessageBox.Show("Record deleted Successfully")
+            LoadDataInGrid()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim id As Integer = Textid.Text
+        Dim command As New SqlCommand("select * from emp_Tab where id = '" & id & "'", con)
+        Dim sds As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sds.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+End Class
